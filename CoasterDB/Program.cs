6ddec72ce8f335dfc8b4bb8955dba65cf14754bb
Jim Falkenstine <jim.fakenstine@gmail.com>
2021-07-04 11:12:49 -0400

    Test commit

---
+++ CoasterDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoasterDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
