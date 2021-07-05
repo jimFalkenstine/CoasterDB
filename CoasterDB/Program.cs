d8f676dd4759c40b97604ff3a6f63b82cd506ab8
Jim Falkenstine <jim.fakenstine@gmail.com>
2021-07-05 15:17:56 -0400

    Added CSV file with Coaster Info.  Added code for Stream Reader.

--- CoasterDB/Program.cs
+++ CoasterDB/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CoasterDB
 {
@@ -6,7 +8,34 @@ namespace CoasterDB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "CoasterCount-ActiveCoasters.csv");
+            var fileContents = ReadCoasterCount(fileName);
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName)) 
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static List<string[]> ReadCoasterCount(string fileName) 
+        {
+            var coasterCount = new List<string[]>();
+            using (var reader = new StreamReader(fileName)) 
+            {
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    coasterCount.Add(values);
+
+                }
+            }
+            return coasterCount;
         }
     }
 }
