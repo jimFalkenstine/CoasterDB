e0c5355be6b6292465ba690836d20f8e832b9e2c
Jim Falkenstine <jim.fakenstine@gmail.com>
2021-07-25 02:52:24 -0400

    Moved Top Ten Lists to their own submenu

--- CoasterDB/Program.cs
+++ CoasterDB/Program.cs
@@ -29,7 +29,7 @@ namespace CoasterDB
 
             //}
 
-            //coasters.Sort();
+            //coasters.OrderBy(c => c.Name).Take(10);
             //foreach (var coaster in coasters) 
             //{
             //    Console.WriteLine("Name: " + coaster.Name);
@@ -50,7 +50,6 @@ namespace CoasterDB
 
         static void MainMenu() 
         {
-            Console.SetBufferSize(80, 1700);
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "coasters.json");
@@ -59,12 +58,10 @@ namespace CoasterDB
             Console.Clear();
             Console.WriteLine("Welcome to CoasterDB - The Roller Coaster Database");
             Console.WriteLine("");
-            Console.WriteLine("Option 1. Top Ten Highest Coasters");
-            Console.WriteLine("Option 2. Top Ten Longest Coasters");
-            Console.WriteLine("Option 3. Top Ten Fastest Coasters");
-            Console.WriteLine("Option 4. Top Ten Coasters With The Most Inversions");
-            Console.WriteLine("Option 5. Coaster Counts");
-            Console.WriteLine("Option 6. Exit");
+            Console.WriteLine("Option 1. Top Ten Coaster Lists");
+            Console.WriteLine("Option 2. Coaster Counts");
+            Console.WriteLine("Option 3. Exit");
+            Console.WriteLine("Option 4. All Coasters");
             Console.WriteLine("");
             Console.WriteLine("Please type the Option Number to navigate");
             
@@ -73,75 +70,37 @@ namespace CoasterDB
             switch (myOptions) 
             {
                 case "1":
-                    Console.Clear();
-                    Console.WriteLine("Top Ten Tallest Coasters");
-                    Console.WriteLine("");
-                    var topTenTallestCoasters = GetTopTenTallestCoasters(coasters);
-                    foreach (var coaster in topTenTallestCoasters)
-                    {
-                        Console.WriteLine("Name: " + coaster.Name);
-                        Console.WriteLine("Park: " + coaster.Park);
-                        Console.WriteLine("Height: " + coaster.Height + " ft");
-                        Console.WriteLine("");
-                    }
-                    Console.WriteLine("Press Enter to return to the Main Menu");
-                    Console.ReadLine();
+                    TopTenListMenu();
                     break;
 
                 case "2":
-                    Console.Clear();
-                    Console.WriteLine("Top Ten Longest Coasters");
-                    Console.WriteLine("");
-                    var topTenLongestCoasters = GetTopTenLongestCoasters(coasters);
-                    foreach (var coaster in topTenLongestCoasters)
-                    {
-                        Console.WriteLine("Name: " + coaster.Name);
-                        Console.WriteLine("Park: " + coaster.Park);
-                        Console.WriteLine("Length: " + coaster.Length + " ft");
-                        Console.WriteLine("");
-                    }
-                    Console.WriteLine("Press Enter to return to the Main Menu");
-                    Console.ReadLine();
+                    CoasterCountMenu();
                     break;
 
                 case "3":
-                    Console.Clear();
-                    Console.WriteLine("Top Ten Fastest Coasters");
-                    Console.WriteLine("");
-                    var topTenFastestCoasters = GetTopTenFastestCoasters(coasters);
-                    foreach (var coaster in topTenFastestCoasters)
-                    {
-                        Console.WriteLine("Name: " + coaster.Name);
-                        Console.WriteLine("Park: " + coaster.Park);
-                        Console.WriteLine("Speed: " + coaster.Speed + " mph");
-                        Console.WriteLine("");
-                    }
-                    Console.WriteLine("Press Enter to return to the Main Menu");
-                    Console.ReadLine();
+                    System.Environment.Exit(1);
                     break;
 
                 case "4":
                     Console.Clear();
-                    Console.WriteLine("Top Ten Coasters With The Most Inversions");
-                    Console.WriteLine("");
-                    var topTenMostInversions = GetTopTenMostInversions(coasters);
-                    foreach (var coaster in topTenMostInversions)
+                    var firstTenCoasters = coasters.OrderBy(c => c.Name).Take(10);
+                    foreach (var coaster in firstTenCoasters)
                     {
                         Console.WriteLine("Name: " + coaster.Name);
                         Console.WriteLine("Park: " + coaster.Park);
+                        Console.WriteLine("Type: " + coaster.Type);
+                        Console.WriteLine("Make: " + coaster.Make);
+                        Console.WriteLine("Length: " + coaster.Length + " ft");
+                        Console.WriteLine("Height: " + coaster.Height + " ft");
+                        Console.WriteLine("Speed: " + coaster.Speed + " mph");
                         Console.WriteLine("Inversions: " + coaster.Inversions);
-                        Console.WriteLine("");
+                        Console.WriteLine("Year: " + coaster.Year);
+                        Console.WriteLine("Status: " + coaster.Status);
+                        Console.WriteLine("Country: " + coaster.Country);
+                        Console.WriteLine(" ");
                     }
                     Console.WriteLine("Press Enter to return to the Main Menu");
                     Console.ReadLine();
-                    break;
-
-                case "5":
-                    CoasterCountMenu();
-                    break;
-
-                case "6":
-                    System.Environment.Exit(1);
                     break;
             }
 
@@ -149,7 +108,6 @@ namespace CoasterDB
         }
         static void CoasterCountMenu()
         {
-            Console.SetBufferSize(80, 1700);
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "coasters.json");
@@ -158,12 +116,14 @@ namespace CoasterDB
             Console.Clear();
             Console.WriteLine("Coaster Counts");
             Console.WriteLine("");
-            Console.WriteLine("Option 1. Coasters by Park");
-            Console.WriteLine("Option 2. Coasters by Type");
-            Console.WriteLine("Option 3. Coasters by Design");
-            Console.WriteLine("Option 4. Coasters by Make");
-            Console.WriteLine("Option 5. Coasters by Status");
-            Console.WriteLine("Option 6. Coasters by Country");
+            Console.WriteLine("Total Coaster Count " + coasters.Count());
+            Console.WriteLine("");
+            Console.WriteLine("Option 1. Coaster Count by Park");
+            Console.WriteLine("Option 2. Coaster Count by Type");
+            Console.WriteLine("Option 3. Coaster Count by Design");
+            Console.WriteLine("Option 4. Coaster Count by Make");
+            Console.WriteLine("Option 5. Coaster Count by Status");
+            Console.WriteLine("Option 6. Coaster Count by Country");
             Console.WriteLine("Option 7. Return to Main Menu");
             Console.WriteLine("");
             Console.WriteLine("Please type the Option Number to navigate");
@@ -174,9 +134,11 @@ namespace CoasterDB
             {
                 case "1":
                     Console.Clear();
-                    Console.WriteLine("Coasters by Park");
+                    Console.WriteLine("Coaster Count by Park");
                     Console.WriteLine("");
                     var coastersByPark = coasters.GroupBy(c => c.Park);
+                    Console.WriteLine("Total Park Count " + coastersByPark.Count());
+                    Console.WriteLine("");
                     foreach (var coaster in coastersByPark)
                     {
                         Console.WriteLine(coaster.Key + " " + coaster.Count());
@@ -188,9 +150,11 @@ namespace CoasterDB
 
                 case "2":
                     Console.Clear();
-                    Console.WriteLine("Coasters by Type");
+                    Console.WriteLine("Coaster Count by Type");
                     Console.WriteLine("");
                     var coastersByType = coasters.GroupBy(c => c.Type);
+                    Console.WriteLine("Total Type Count " + coastersByType.Count());
+                    Console.WriteLine("");
                     foreach (var coaster in coastersByType)
                     {
                         Console.WriteLine(coaster.Key + " " + coaster.Count());
@@ -202,9 +166,11 @@ namespace CoasterDB
 
                 case "3":
                     Console.Clear();
-                    Console.WriteLine("Coasters by Design");
+                    Console.WriteLine("Coaster Count by Design");
                     Console.WriteLine("");
                     var coastersByDesign = coasters.GroupBy(c => c.Design);
+                    Console.WriteLine("Total Design Count " + coastersByDesign.Count());
+                    Console.WriteLine("");
                     foreach (var coaster in coastersByDesign)
                     {
                         Console.WriteLine(coaster.Key + " " + coaster.Count());
@@ -216,9 +182,11 @@ namespace CoasterDB
 
                 case "4":
                     Console.Clear();
-                    Console.WriteLine("Coasters by Make");
+                    Console.WriteLine("Coaster Count by Make");
                     Console.WriteLine("");
                     var coastersByMake = coasters.GroupBy(c => c.Make);
+                    Console.WriteLine("Total Make Count " + coastersByMake.Count());
+                    Console.WriteLine("");
                     foreach (var coaster in coastersByMake)
                     {
                         Console.WriteLine(coaster.Key + " " + coaster.Count());
@@ -230,9 +198,11 @@ namespace CoasterDB
 
                 case "5":
                     Console.Clear();
-                    Console.WriteLine("Coasters by Status");
+                    Console.WriteLine("Coaster Count by Status");
                     Console.WriteLine("");
                     var coastersByStatus = coasters.GroupBy(c => c.Status);
+                    Console.WriteLine("Total Status Count " + coastersByStatus.Count());
+                    Console.WriteLine("");
                     foreach (var coaster in coastersByStatus)
                     {
                         Console.WriteLine(coaster.Key + " " + coaster.Count());
@@ -244,9 +214,11 @@ namespace CoasterDB
 
                 case "6":
                     Console.Clear();
-                    Console.WriteLine("Coasters by Country");
+                    Console.WriteLine("Coaster Count by Country");
                     Console.WriteLine("");
                     var coastersByCountry = coasters.GroupBy(c => c.Country);
+                    Console.WriteLine("Total Country Count " + coastersByCountry.Count());
+                    Console.WriteLine("");
                     foreach (var coaster in coastersByCountry)
                     {
                         Console.WriteLine(coaster.Key + " " + coaster.Count());
@@ -262,6 +234,100 @@ namespace CoasterDB
             }
             CoasterCountMenu();
         }
+
+        static void TopTenListMenu()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "coasters.json");
+            var coasters = DeserializeCoasters(fileName);
+
+            Console.Clear();
+            Console.WriteLine("Top Ten Coaster Lists");
+            Console.WriteLine("");
+            Console.WriteLine("Option 1. Top Ten Tallest Coasters");
+            Console.WriteLine("Option 2. Top Ten Longest Coasters");
+            Console.WriteLine("Option 3. Top Ten Fastest Coasters");
+            Console.WriteLine("Option 4. Top Ten Coasters With The Most Inversions");
+            Console.WriteLine("Option 5. Return to Main Menu");
+            Console.WriteLine("");
+            Console.WriteLine("Please type the Option Number to navigate");
+
+            var myTopTenListOptions = Console.ReadLine();
+
+            switch (myTopTenListOptions)
+            {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("Top Ten Tallest Coasters");
+                    Console.WriteLine("");
+                    var topTenTallestCoasters = GetTopTenTallestCoasters(coasters);
+                    foreach (var coaster in topTenTallestCoasters)
+                    {
+                        Console.WriteLine("Name: " + coaster.Name);
+                        Console.WriteLine("Park: " + coaster.Park);
+                        Console.WriteLine("Height: " + coaster.Height + " ft");
+                        Console.WriteLine("");
+                    }
+                    Console.WriteLine("Press Enter to return to the Top Ten Coaster List Menu");
+                    Console.ReadLine();
+                    break;
+
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("Top Ten Longest Coasters");
+                    Console.WriteLine("");
+                    var topTenLongestCoasters = GetTopTenLongestCoasters(coasters);
+                    foreach (var coaster in topTenLongestCoasters)
+                    {
+                        Console.WriteLine("Name: " + coaster.Name);
+                        Console.WriteLine("Park: " + coaster.Park);
+                        Console.WriteLine("Speed: " + coaster.Length + " ft");
+                        Console.WriteLine("");
+                    }
+                    Console.WriteLine("Press Enter to return to the Top Ten Coaster List Menu");
+                    Console.ReadLine();
+                    break;
+
+                case "3":
+                    Console.Clear();
+                    Console.WriteLine("Top Ten Fastest Coasters");
+                    Console.WriteLine("");
+                    var topTenFastestCoasters = GetTopTenFastestCoasters(coasters);
+                    foreach (var coaster in topTenFastestCoasters)
+                    {
+                        Console.WriteLine("Name: " + coaster.Name);
+                        Console.WriteLine("Park: " + coaster.Park);
+                        Console.WriteLine("Speed: " + coaster.Speed + " mph");
+                        Console.WriteLine("");
+                    }
+                    Console.WriteLine("Press Enter to return to the Top Ten Coaster List Menu");
+                    Console.ReadLine();
+                    break;
+
+                case "4":
+                    Console.Clear();
+                    Console.WriteLine("Top Ten Coasters With The Most Inversions");
+                    Console.WriteLine("");
+                    var topTenMostInversions = GetTopTenMostInversions(coasters);
+                    foreach (var coaster in topTenMostInversions)
+                    {
+                        Console.WriteLine("Name: " + coaster.Name);
+                        Console.WriteLine("Park: " + coaster.Park);
+                        Console.WriteLine("Inversions: " + coaster.Inversions);
+                        Console.WriteLine("");
+                    }
+                    Console.WriteLine("Press Enter to return to the Top Ten Coaster List Menu");
+                    Console.ReadLine();
+                    break;
+
+                case "5":
+                    MainMenu();
+                    break;
+            }
+            TopTenListMenu();
+        }
+
         public static List<Coaster> DeserializeCoasters(string fileName)
         {
             var coasters = new List<Coaster>();
